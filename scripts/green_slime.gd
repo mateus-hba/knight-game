ea99eef7142a6fa12b2fab81f19634d87fe15811
mateus-hba <mateushba@gmail.com>
2026-01-11 23:34:39 -0300

    Adicionando o primeiro inimigo
    Adicionando killzone e slime verde com movimentação básica alterando a animação ao prever colisão com paredes

---
+++ scripts/green_slime.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+const SPEED	= 55
+
+var direction = 1
+@onready var ray_cast_right = $RayCastRight
+@onready var ray_cast_left = $RayCastLeft
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _process(delta):
+	if ray_cast_right.is_colliding():
+		direction = -1
+		animated_sprite.flip_h = true
+	elif ray_cast_left.is_colliding():
+		direction = 1
+		animated_sprite.flip_h = false
+	
+	position.x += direction * SPEED * delta
